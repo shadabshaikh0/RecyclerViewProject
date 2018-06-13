3c84038f5c1e0b6ca2f085eb1ce464cefb4253ec
shadab <sshaikh.ss319@gmail.com>
2018-06-13 23:21:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shadab/recyclerviewproject/MyAdapter.java
@@ -0,0 +1,75 @@
+package com.example.shadab.recyclerviewproject;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    private List<ListItem> listItems;
+    private Context context;
+
+    public MyAdapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final ListItem listItem = listItems.get(position);
+        holder.textViewHead.setText(listItem.getHead());
+        holder.textViewDesc.setText(listItem.getDesc());
+        Picasso.get()
+                .load(listItem.getImageUrl())
+                .into(holder.imageView);
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "You cliked"+listItem.getHead(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView textViewHead;
+        public TextView textViewDesc;
+        public ImageView imageView;
+        public LinearLayout linearLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textViewHead = (TextView)itemView.findViewById(R.id.textViewHead);
+            textViewDesc = (TextView)itemView.findViewById(R.id.textViewDesc);
+            imageView    = (ImageView)itemView.findViewById(R.id.imageView);
+            linearLayout = (LinearLayout)itemView.findViewById(R.id.linearLayout);
+
+        }
+    }
+}
